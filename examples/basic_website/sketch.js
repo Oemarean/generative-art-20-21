8492c6ce01f2bc3e9cfb6b4f7617a72b394e2430
Oemarean <Oemaralkatiri@gmail.com>
2021-04-21 11:37:22 +0200

    test 1

--- examples/basic_website/sketch.js
+++ examples/basic_website/sketch.js
@@ -1,14 +1,48 @@
 /* 👇 Start writing your p5.js code here */
 
+let sizewidth = 25
+let sizeheight = 25
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(sizewidth*30, sizeheight*30);
 }
 
 function draw() {
-  background(220);
-  ellipse(width / 2, height / 2, 50, 50);
+  background(255);
+  for(y = 0; y < sizeheight; y++){
+    for(x = 0; x < sizewidth; x++){
+      if(getItem(x + ":" + y) == null){
+          storeItem(x + ":" + y, false)
+        }
+      if(getItem(x + ":" + y) == true){
+        fill(155,0,0)
+      }
+      rect(x*30, y*30, 30, 30)
+      noFill()
+    }
+  }
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+function mouseClicked(){
+  for(y = 0; y < sizeheight; y++){
+    for(x = 0; x < sizewidth; x++){
+      if(mouseX < x*30+30 && mouseX > x*30 && mouseY < y*30+30 && mouseY > y*30){
+          storeItem(x + ":" + y, true)
+      }
+    }
+  }
+}
+
+function keyTyped(){
+  if(key == " "){
+    clearStorage()
+  }
+  if(key == "a"){
+    for(y = 0; y < sizeheight; y++){
+      for(x = 0; x < sizewidth; x++){
+        if(mouseX < x*30+30 && mouseX > x*30 && mouseY < y*30+30 && mouseY > y*30){
+          storeItem(x + ":" + y, false)
+        }
+      }
+    }
+  }
 }
